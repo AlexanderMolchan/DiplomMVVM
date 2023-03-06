435265bb6411a25bd4970087f64f7d5f08d3d67b
Alexander Molchan <79983103+AlexanderMolchan@users.noreply.github.com>
2023-03-06 15:04:58 +0300

    TabBarExtension

---
+++ DiplomProject/Extensions/TabBarController + Extension.swift
@@ -0,0 +1,37 @@
+//
+//  TabBarController + Extension.swift
+//  DiplomProject
+//
+//  Created by Александр Молчан on 6.03.23.
+//
+
+import UIKit
+
+extension UITabBarController {
+    var tabBarHidden: Bool {
+        tabBar.frame.origin.y >= UIScreen.main.bounds.height
+    }
+    
+    func setTabBarHidden(_ hidden: Bool, animated: Bool) {
+        guard let vc = selectedViewController else { return }
+        guard tabBarHidden != hidden else { return }
+        
+        let frame = self.tabBar.frame
+        let height = frame.size.height
+        let offsetY = hidden ? height : -height
+
+        UIViewPropertyAnimator(duration: animated ? 0.5 : 0, curve: .easeOut) {
+            self.tabBar.frame = self.tabBar.frame.offsetBy(dx: 0, dy: offsetY)
+            self.selectedViewController?.view.frame = CGRect(
+                x: 0,
+                y: 0,
+                width: vc.view.frame.width,
+                height: vc.view.frame.height + offsetY
+            )
+            self.view.setNeedsDisplay()
+            self.view.layoutIfNeeded()
+        }
+        .startAnimation()
+    }
+}
+
